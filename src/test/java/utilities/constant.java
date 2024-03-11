f1c43591232175c1a1b91ce1b712fc11c6b1fc5c
JayaSri Banagiri <jayasri@192.168.0.26>
2024-03-11 15:42:35 +1100

    First Commit

---
+++ src/test/java/utilities/constant.java
@@ -0,0 +1,20 @@
+package utilities;
+
+public class constant {
+
+		
+		protected static String apiKey ="eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJzaGFua2FyMTMyNCIsImVtYWlsIjoic2hhbmthcjEzMjRAZ21haWwuY29tIiwiaWF0IjoxNjUzMDA2MTk0LCJleHAiOjE2NTMyNjUzOTR9.2Cp3gUBukLV75xtiYmOZgAfivny419AH1uRem2O5XjuRux9P2AwD2kqYH3c7U54Al-0V_2XWLLkA8e5gCLv6hw";
+//				"0c295fc2-a846-4086-8409-1bc4852ea7af";
+		
+		protected String baseURI = "https://supervillain.herokuapp.com";
+		protected String query;
+		protected String token;	
+		protected String existingUsername = "JayasriBanagiri";
+		protected String newUser = "Jayasri";
+		protected String password = "test123$";
+		protected String incorrectPassword = "incorrect";
+		
+		protected int score = 0;
+		protected int scoreUpdate = 1500;
+	
+}
